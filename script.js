0e733a8383a0d724dc1fc5526528dab136f0e6e6
Tobi_03 <m4sk3d.t0bi@gmail.com>
2026-01-11 23:16:56 +0530

    made changes

---
+++ script.js
@@ -0,0 +1,15 @@
+const questions = [
+  "What is the capital of India?",
+  "Which planet is known as the Red Planet?",
+  "Who is known as the Father of the Nation in India?",
+  "How many days are there in a week?",
+  "Which is the largest ocean in the world?"
+];
+
+const answers = [
+  "New Delhi",
+  "Mars",
+  "Mahatma Gandhi",
+  "7",
+  "Pacific Ocean"
+];
