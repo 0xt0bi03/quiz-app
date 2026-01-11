eaaea782f5a284dfc108f1d090bf72e6732f8e2e
Tobi_03 <m4sk3d.t0bi@gmail.com>
2026-01-11 23:24:54 +0530

    made changes

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ const questions = [
   "Which is the largest ocean in the world?"
 ];
 
+const options = [
+  ["Mumbai", "New Delhi", "Kolkata", "Chennai"],
+  ["Earth", "Venus", "Mars", "Jupiter"],
+  ["Jawaharlal Nehru", "Mahatma Gandhi", "Sardar Patel", "Subhash Chandra Bose"],
+  ["5", "6", "7", "8"],
+  ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"]
+];
+
 const answers = [
   "New Delhi",
   "Mars",
@@ -16,4 +24,20 @@ const answers = [
 
 
 let question = document.querySelector('#question');
+let question = document.querySelector('#question');
+let question = document.querySelector('#question');
+let question = document.querySelector('#question');
+
+function ask(number)
+{
+    
+}
+
+
 console.log(question.textContent);
+
+// questions.forEach(element =>
+// {
+
+    
+// });
